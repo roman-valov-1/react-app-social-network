5d64ee0aa1323c5951ce7ec3d8332d2525255073
roman-valov-1 <62258593+roman-valov-1@users.noreply.github.com>
2021-02-21 12:47:10 +0300

    add react-final-form to project(in login.jsx)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Form, Field } from 'react-final-form';
+
+
+const LoginForm = (props) => (
+   <Form
+      onSubmit={props.onSubmit}
+      render={({ handleSubmit }) => (
+         <form onSubmit={handleSubmit}>
+            <div>
+               <Field name="Login" component="input" placeholder="Login" />
+            </div>
+            <div>
+               <Field name="Password" component="input" placeholder="Password" />
+            </div>
+            <div>
+               <Field name="rememberMe" component="input" type="checkbox" /> remember me
+            </div>
+            <div>
+               <button type="submit" >Sign in</button>
+            </div>
+         </form>
+      )}
+   />
+)
 
 const Login = (props) => {
-   return <h1>Login</h1>
+   const onSubmit = ( formData ) => {
+      console.log(formData);
+   }
+   return <div>
+      <h1>Login</h1>
+      <LoginForm onSubmit={onSubmit}/>
+   </div>
 }
 
 export default Login;
