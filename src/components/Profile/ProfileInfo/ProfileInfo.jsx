3eee81597432f451655e6c604dca1a919dca13bc
roman-valov-1 <62258593+roman-valov-1@users.noreply.github.com>
2021-02-26 20:46:47 +0300

    create ProfileStatusWithHooks (useState ,useEffect)

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,6 @@
 import classes from './ProfileInfo.module.css';
 import Preloader from './../../Common/Preloader/Preloader';
-import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
    if(!props.profile){
@@ -10,7 +10,7 @@ const ProfileInfo = (props) => {
       <div>
          <div className={classes.descriptionBlock}>
             <img className={classes.image} src={props.profile.photos.large} alt=""/>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
          </div>
       </div>   
    );
