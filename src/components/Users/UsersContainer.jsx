db98a07e9b7660a65ab5c3c1da74574b9e88287e
roman-valov-1 <62258593+roman-valov-1@users.noreply.github.com>
2021-02-26 18:01:16 +0300

    add selectors for UsersContainer component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -10,6 +10,14 @@ import { connect } from 'react-redux';
 import Users from './Users';
 import Preloader from './../Common/Preloader/Preloader';
 import { compose } from 'redux';
+import {
+   getUsersData,
+   getPageSize,
+   getTotalUsersCount,
+   getCurrentPage,
+   getIsFetching,
+   getFollowingInProgress
+} from './../../Redux/selectors';
 
 class UsersContainer extends React.Component {
    componentDidMount() {
@@ -39,14 +47,25 @@ class UsersContainer extends React.Component {
    }
 }
 
+// let mapStateToProps = (state) => {
+//    return {
+//       users: state.usersPage.users,
+//       pageSize: state.usersPage.pageSize,
+//       totalUsersCount: state.usersPage.totalUsersCount,
+//       currentPage: state.usersPage.currentPage,
+//       isFetching: state.usersPage.isFetching,
+//       followingInProgress: state.usersPage.followingInProgress
+//    }
+// }
+
 let mapStateToProps = (state) => {
    return {
-      users: state.usersPage.users,
-      pageSize: state.usersPage.pageSize,
-      totalUsersCount: state.usersPage.totalUsersCount,
-      currentPage: state.usersPage.currentPage,
-      isFetching: state.usersPage.isFetching,
-      followingInProgress: state.usersPage.followingInProgress
+      users: getUsersData(state),
+      pageSize: getPageSize(state),
+      totalUsersCount: getTotalUsersCount(state),
+      currentPage: getCurrentPage(state),
+      isFetching: getIsFetching(state),
+      followingInProgress: getFollowingInProgress(state)
    }
 }
 
