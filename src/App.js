9a4cbbae7a9a9b8dc08494bc95eeeadbcb0a4ccb
roman-valov-1 <62258593+roman-valov-1@users.noreply.github.com>
2021-04-22 22:08:48 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
 		}
 
 		return (
-			<HashRouter basename={process.env.PUBLIC_URL}>
+			<HashRouter>
 				<div className="app-wrapper">
 					<HeaderContainer />
 					<div className="app-content-wrapper">
